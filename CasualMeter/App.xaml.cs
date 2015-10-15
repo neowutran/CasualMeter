244e6bd7738df84546253eea0c5797725cb3d524
Lunyx <dev@lunyx.net>
2015-10-14 20:02:04 -0400

    Added basic shell

--- CasualMeter/App.xaml.cs
+++ CasualMeter/App.xaml.cs
@@ -5,13 +5,38 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Lunyx.Common.UI.Wpf;
 
 namespace CasualMeter
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : ISingleInstanceApp
     {
+        private const string Unique = "283055BC-D5AC-46CC-B1A9-51C053BB9028";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+            else { }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // ...
+            return true;
+        }
     }
 }

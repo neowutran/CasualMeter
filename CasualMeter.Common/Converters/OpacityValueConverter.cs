9c2fc0e9a0f9129ee62c1d74b337f41566e97a78
Lunyx <dev@lunyx.net>
2015-12-27 04:29:37 -0500

    Opacity fix

---
+++ CasualMeter.Common/Converters/OpacityValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CasualMeter.Common.Converters
+{
+    public class OpacityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || (value != null && !(value is double)))
+                throw new ArgumentException($"Invalid arguments passed to {nameof(OpacityValueConverter)}.");
+
+            var modifiedValue = (double)value * 1;
+            return modifiedValue > 1 ? 1 : modifiedValue < 0.5 ? 0.5 : modifiedValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

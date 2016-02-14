c6f439493ed6ecde9dbd615b42b26e4515539de3
Lunyx <dev@lunyx.net>
2016-02-14 12:57:46 -0500

    Renamed command/method

--- CasualMeter/ShellViewModel.cs
+++ CasualMeter/ShellViewModel.cs
@@ -172,9 +172,9 @@ namespace CasualMeter
 
         #region Commands
 
-        public RelayCommand ToggleIsPinned
+        public RelayCommand ToggleIsPinnedCommand
         {
-            get { return GetProperty(getDefault: () => new RelayCommand(vToggleIsPinned)); }
+            get { return GetProperty(getDefault: () => new RelayCommand(ToggleIsPinned)); }
             set { SetProperty(value); }
         }
 
@@ -190,12 +190,6 @@ namespace CasualMeter
             set { SetProperty(value); }
         }
         #endregion
-
-        public void vToggleIsPinned()
-        {
-            IsPinned = !IsPinned;
-            ProcessHelper.Instance.ForceVisibilityRefresh();
-        }
 
         private object _snifferLock = new object();
 
@@ -333,6 +327,12 @@ namespace CasualMeter
         private void ClearEncounters()
         {
             ArchivedDamageTrackers.Clear();
+        }
+
+        private void ToggleIsPinned()
+        {
+            IsPinned = !IsPinned;
+            ProcessHelper.Instance.ForceVisibilityRefresh();
         }
     }
 }

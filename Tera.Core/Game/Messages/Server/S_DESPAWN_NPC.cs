cd4751e2ef8a8826c4144323d2be96c5d6c58fa1
Gl0 <ab@glk.spb.ru>
2016-03-14 00:22:48 +0300

    Fix raw sockets not capturing on PPP interfaces.

--- Tera.Core/Game/Messages/Server/S_DESPAWN_NPC.cs
+++ Tera.Core/Game/Messages/Server/S_DESPAWN_NPC.cs
@@ -5,11 +5,12 @@
         internal SDespawnNpc(TeraMessageReader reader) : base(reader)
         {
             NPC = reader.ReadEntityId();
-            reader.Skip(12);
+            Position = reader.ReadVector3f();
             Dead = reader.ReadByte() == 5; // 1 = move out of view, 5 = death
         }
 
         public EntityId NPC { get; }
+        public Vector3f Position { get; private set; }
         public bool Dead { get; }
     }
 }
